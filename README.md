d8240554365b7e9eeaeb77e9110e6e230079bead
Andrew Berezovsky <andrew.berezovsky@gmail.com>
2013-01-30 18:54:20 +0400

    Enhanced and rewritten version Decoupled buttons from general playlist building Buttons can be rearranged as you wish Added loop and shuffle buttons No need to overwrite MediaElement.js controls.png

--- README.md
+++ README.md
@@ -1,45 +1,46 @@
-mep-feature-playlist -  A playplist plugin for MediaElementjs
-====================
+mep-feature-playlist -  A playlist plugin for MediaElement.js
 
 Usage:
-----------
-1-
-Download **[mediaElemntjs] (http://github.com/johndyer/mediaelement/zipball/master)**
 
-2-
-Download **[mep-feature-playlist] (https://github.com/JeyKeu/mep-feature-playlist/zipball/master)**
+1 - Download **[MediaElement.js] (http://mediaelementjs.com/)**
 
-3- Include 
+2 - Download **[mep-feature-playlist] (https://github.com/duozersk/mep-feature-playlist/archive/master.zip)**
+
+3 - Include
 
 - **mediaelement-and-player.js**
-
+- **mediaelementplayer.min.css**
 - **mep-feature-playlist.js**
+- **mep-feature-playlist.css**
 
-4- Add this script to your page:
+4 - Add this code to your page:
 
     <script>
     $(function(){
-    
-            $('audio,video').mediaelementplayer({
+        $('audio,video').mediaelementplayer({
             alwaysShowControls:false, 
-            features: ['playpause', 'playlist', 'progress', 'volume', 'sourcechooser']
-            });
-    
+            features: ['playlistfeature', 'prevtrack', 'playpause', 'nexttrack', 'loop', 'shuffle', 'playlist', 'current', 'progress', 'duration', 'volume']
+        });
     });
     </script>
+    
+    playlistfeature - general feature to enable playlist functionality; it just builds the internal playlist layer, it should be present if you want to use playlist
+    prevtrack - button to play the previous track in the playlist
+    nexttrack - button to play the next track in the playlist
+    loop - toggle to turn repeat on or off
+    shuffle - toggle to turn shuffle on or off - DOESN'T WORK YET
+    playlist - playlist button to show/hide playlist
 
-5- Add the audio tag and your tracks as follows:
+5 - Add the audio tag and your tracks:
 
-    <audio id="player2" controls="controls">
-        <source src="media/AirReview-Landmarks-02-ChasingCorporate.mp3" type="audio/mp3"/>
-        <source src="media/framing.mp3" type="audio/mp3"/>
+    <audio id="player2" controls="controls" autoplay="autoplay">
+        <source src="media/AirReview-Landmarks-02-ChasingCorporate.mp3" title="Chasing Corporate" type="audio/mpeg"/>
+        <source src="media/framing.mp3" title="Framing" type="audio/mpeg"/>
     </audio>
-    
-    
-You will also need to replace the controls.png with the on included with this plugin. 
 
+
+==NOT UPDATED SECTIONS==
 Screenshots
-=========================
 
 **Default Collapsed**
 
@@ -60,4 +61,3 @@ When the player is loaded with the **playist** feature, it will add playlist tog
 More options and installation instructions related to MediaElementjs itself can be found [here] (http://mediaelementjs.com/#installation).
 
 ###[Demo] (http://projects.junaidbaloch.com/mep-feature-playlist/)
-
--- mep-feature-playlist.js
+++ mep-feature-playlist.js
@@ -1,160 +1,196 @@
 /**
- * MediaElement Playlist Feature (plugin). 
- * @author Junaid Qadir Baloch <shekhanzai.baloch@gmail.com>
- * Dual licensed under the MIT or GPL Version 2 licenses.
+ * @file MediaElement Playlist Feature (plugin).
+ * @author Andrew Berezovsky <andrew.berezovsky@gmail.com>
+ * Twitter handle: duozersk
+ * @author Original author: Junaid Qadir Baloch <shekhanzai.baloch@gmail.com>
  * Twitter handle: jeykeu
-*/
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ */
+
 (function($) {
-    $.extend(mejs.MepDefaults, {
-        playlistText: 'Show/Hide Playlist',
-        nextText: 'Next Track',
-        prevText: 'Previous Track'
-    });
-    
-    // PLAYLIST WINDOW
-    $.extend(MediaElementPlayer.prototype, {        
-        buildplaylist: function(player, controls, layers, media) {            
-            var 
-            t = this,
-            // PREVIOUS TRACK BUTTON
-            prevButton = $('<div class="mejs-button mejs-prev-button mejs-prev">' +
-                '<button type="button" aria-controls="' + t.id + '" title="' + t.options.prevText + '"></button>' +
-                '</div>')
-            .appendTo(controls)
-            .click(function(e){
-                e.preventDefault();
-                loadPrevTrack();
-                
-            }),
-            // NEXT TRACK BUTTON
-            nextButton = $('<div class="mejs-button mejs-next-button mejs-next">' +
-                '<button type="button" aria-controls="' + t.id + '" title="' + t.options.nextText + '"></button>' +
-                '</div>')
-            .appendTo(controls)
-            .click(function(e){
-                e.preventDefault();
-                loadNextTrack();
-            }),
-            playlistWindow = 
-            $('<div class="mejs-layer mejs-playlistWindow">' +
-                '<ul class="mejs"></ul>' +
-                '</div>').appendTo(layers).hide(),
-            // PLAYLIST BUTTON
-            playlistButton = 
-            $('<div class="mejs-button mejs-playlist-button mejs-show-playlist" >' +
-                '<button type="button" aria-controls="' + t.id + '" title="' + t.options.playlistText + '"></button>' +
-                '</div>')
-            .appendTo(controls)
-            .click(function(e) {
-                e.preventDefault();
-                if(typeof media.playlist == "undefined"){
-                    media.playlist = false;
-                }
-                if (media.playlist) {
-                    playlistWindow.hide();
-                    playlistButton.removeClass("mejs-hide-playlist").addClass("mejs-show-playlist");
-                    media.playlist = false;
-                } else {
-                    media.playlist = true;
-                    playlistWindow.show();
-                    playlistButton.removeClass("mejs-show-playlist").addClass("mejs-hide-playlist");
-                }
-                return false;
-            });
-            
-            getTrackName = function(trackUrl){
-                if($.trim(trackUrl) == "" ){
-                    return;
-                }
-                var trackUrlParts = trackUrl.split("/");
-                if(trackUrlParts.length > 0){
-                    return trackUrlParts[trackUrlParts.length-1];
-                }else{
-                    return;
-                }
-            };
-            var tracks = [];
-            
-            $(".mejs-mediaelement audio source, .mejs-mediaelement video source").each(function(){
-                if($.trim(this.src) != ""){
-                    var track = {
-                        source: this.src, 
-                        name: ''
-                    };
-                    tracks.push(track);
-                }
-            });
-            
-            for(var track in tracks){                
-                var trackFile = getTrackName(tracks[track].source);
-                tracks[track].name = trackFile;
-                tracks[track].name = decodeURIComponent(tracks[track].name);
-                $(".mejs-layers .mejs-playlistWindow > ul").append('<li><a href="javascript:;" data-url="' + tracks[track].source + '" title="' + tracks[track].name + '">' + tracks[track].name + '</a></li>');
-            };
-            
-            var loadNextTrack = function (){
-             
-                var nxt = $(".mejs-layers .mejs-playlistWindow > ul > li > a.current").parent().next();
-                if(nxt.length <1){
-                    nxt = $(".mejs-layers .mejs-playlistWindow > ul > li:first-child > a").parent();
-                }
-                var url =nxt.children("a").attr("data-url");
-                $(".mejs-layers .mejs-playlistWindow > ul li a").removeClass("current");
-                nxt.children("a").addClass("current");
-                loadTrack(media, url);
-            };
-            
-            var loadPrevTrack = function (){
-                var prev = $(".mejs-layers .mejs-playlistWindow > ul > li > a.current").parent().prev();
-                if(prev.length <1){
-                    prev = $(".mejs-layers .mejs-playlistWindow > ul > li:last-child > a").parent();
-                }
-                var url = prev.children("a").attr("data-url");
-                $(".mejs-layers .mejs-playlistWindow > ul li a").removeClass("current");
-                prev.children("a").addClass("current");
-                loadTrack(media, url);
-            };
-            var loadTrack = function(media, url){
-                if(media.src != url){
-                    media.pause();
-                    media.setSrc(url);
-                    media.play();
-                }else{
-                    if(media.paused){
-                        media.play();
-                    }    
-                }
-            };
-            
-            /**
-             * When play is clicked, highlight the track being played in the 
-             * playlist window
-             */
-            media.addEventListener("play",function(e){
-                $(".mejs-layers .mejs-playlistWindow > ul li a").removeClass("current");
-                $(".mejs-layers .mejs-playlistWindow > ul li a").each(function(){
-                    if(media.src == $(this).attr("data-url")){
-                        $(this).addClass("current");
-                        return;
-                    }
-                });
-                
-                
-            }, true);
-            
-            $(".mejs-layers .mejs-playlistWindow > ul li a").on("click",function(){                
-                try{                   
-                    loadTrack(media, $(this).attr("data-url"));
-                    
-                    $(".mejs-layers .mejs-playlistWindow > ul li a").removeClass("current");
-                    $(this).addClass("current");
-                }
-                catch(e){
-                    console.log(e);
-                }
-                return;
-            });
+  $.extend(mejs.MepDefaults, {
+    loopText: 'Repeat On/Off',
+    shuffleText: 'Shuffle On/Off',
+    nextText: 'Next Track',
+    prevText: 'Previous Track',
+    playlistText: 'Show/Hide Playlist'
+  });
+
+  // LOOP TOGGLE
+  MediaElementPlayer.prototype.buildloop = function(player, controls, layers, media) {
+    //console.log(player); console.log(controls); console.log(layers); console.log(media);
+    var loop = $('<div class="mejs-button mejs-loop-button ' + ((player.options.loop) ? 'mejs-loop-on' : 'mejs-loop-off') + '">' +
+      '<button type="button" aria-controls="' + player.id + '" title="' + player.options.loopText + '"></button>' +
+      '</div>')
+      // append it to the toolbar
+      .appendTo(controls)
+      // add a click toggle event
+      .click(function(e) {
+        player.options.loop = !player.options.loop;
+        if (player.options.loop) {
+          loop.removeClass('mejs-loop-off').addClass('mejs-loop-on');
+          //media.setAttribute('loop', 'loop');
         }
+        else {
+          loop.removeClass('mejs-loop-on').addClass('mejs-loop-off');
+          //media.removeAttribute('loop');
+        }
+      });
+  };
+  // SHUFFLE TOGGLE
+  MediaElementPlayer.prototype.buildshuffle = function(player, controls, layers, media) {
+    //console.log(player); console.log(controls); console.log(layers); console.log(media);
+    var shuffle = $('<div class="mejs-button mejs-shuffle-button ' + ((player.options.shuffle) ? 'mejs-shuffle-on' : 'mejs-shuffle-off') + '">' +
+      '<button type="button" aria-controls="' + player.id + '" title="' + player.options.shuffleText + '"></button>' +
+      '</div>')
+      // append it to the toolbar
+      .appendTo(controls)
+      // add a click toggle event
+      .click(function(e) {
+        player.options.shuffle = !player.options.shuffle;
+        if (player.options.shuffle) {
+          shuffle.removeClass('mejs-shuffle-off').addClass('mejs-shuffle-on');
+        }
+        else {
+          shuffle.removeClass('mejs-shuffle-on').addClass('mejs-shuffle-off');
+        }
+      });
+  };
+  // PREVIOUS TRACK BUTTON
+  MediaElementPlayer.prototype.buildprevtrack = function(player, controls, layers, media) {
+    var prevButton = $('<div class="mejs-button mejs-prevtrack-button mejs-prevtrack">' +
+      '<button type="button" aria-controls="' + player.id + '" title="' + player.options.prevText + '"></button>' +
+      '</div>')
+      .appendTo(controls)
+      .click(function(e){
+        player.playPrevTrack();
+      });
+  };
+  // NEXT TRACK BUTTON
+  MediaElementPlayer.prototype.buildnexttrack = function(player, controls, layers, media) {
+    var nextButton = $('<div class="mejs-button mejs-nexttrack-button mejs-nexttrack">' +
+      '<button type="button" aria-controls="' + player.id + '" title="' + player.options.nextText + '"></button>' +
+      '</div>')
+      .appendTo(controls)
+      .click(function(e){
+        player.playNextTrack();
+      });
+  };
+  // PLAYLIST BUTTON
+  MediaElementPlayer.prototype.buildplaylist = function(player, controls, layers, media) {
+    //console.log(player); console.log(controls); console.log(layers); console.log(media);
+    var playlistButton = $('<div class="mejs-button mejs-playlist-button mejs-show-playlist">' +
+      '<button type="button" aria-controls="' + player.id + '" title="' + player.options.playlistText + '"></button>' +
+      '</div>')
+      .appendTo(controls)
+      .click(function(e) {
+        if (typeof media.playlist == 'undefined') {
+          media.playlist = false;
+        }
+        if (media.playlist) {
+          layers.children('.mejs-playlist').hide();
+          playlistButton.removeClass('mejs-hide-playlist').addClass('mejs-show-playlist');
+          media.playlist = false;
+        }
+        else {
+          layers.children('.mejs-playlist').show();
+          playlistButton.removeClass('mejs-show-playlist').addClass('mejs-hide-playlist');
+          media.playlist = true;
+        }
+      });
+  };
+  
+  // PLAYLIST WINDOW
+  MediaElementPlayer.prototype.buildplaylistfeature = function(player, controls, layers, media) {
+    //console.log(player); console.log(controls); console.log(layers); console.log(media);
+    var playlist = $('<div class="mejs-playlist mejs-layer">' +
+      '<ul class="mejs"></ul>' +
+      '</div>')
+      .appendTo(layers).hide();
+    var getTrackName = function(trackUrl) {
+      var trackUrlParts = trackUrl.split("/");
+      if (trackUrlParts.length > 0) {
+        return decodeURIComponent(trackUrlParts[trackUrlParts.length-1]);
+      }
+      else {
+        return '';
+      }
+    };
+
+    // calculate tracks and build playlist
+    var tracks = [];
+    $(media).children('source').each(function(index, element) {
+      if ($.trim(this.src) != '') {
+        var track = {};
+        track.source = $.trim(this.src);
+        if ($.trim(this.title) != '') {
+          track.name = $.trim(this.title);
+        }
+        else {
+          track.name = getTrackName(track.source);
+        }
+        tracks.push(track);
+      }
     });
-	
-})(mejs.$);
+    for (var track in tracks) {
+      layers.find('.mejs-playlist > ul').append('<li data-url="' + tracks[track].source + '" title="' + tracks[track].name + '">' + tracks[track].name + '</li>');
+    }
+    
+    // set the first track as current
+    layers.find('li:first').addClass('current played');
+    // play track from playlist when clicking it
+    layers.find('.mejs-playlist > ul li').click(function(e) {
+      if (!$(this).hasClass('current')) {
+        player.playTrack($(this).attr('data-url'));
+        $(this).addClass('current').siblings().removeClass('current');
+      }
+      else {
+        player.play();
+      }
+    });
+
+    player.playNextTrack = function() {
+      var current = layers.find('.mejs-playlist > ul > li.current');
+      /*var notplayed = layers.find('.mejs-playlist > ul > li').not('.played');
+      if (notplayed.length < 1) {
+        current.removeClass('played').siblings().removeClass('played');
+        notplayed = layers.find('.mejs-playlist > ul > li').not('.played');
+      }*/
+      var nxt = current.next();
+      if (nxt.length < 1 && player.options.loop) {
+        nxt = current.siblings().first();
+      }
+      if (nxt.length == 1) {
+        nxt.addClass('current played').siblings().removeClass('current');
+        player.playTrack(nxt.attr('data-url'));
+      }
+    };
+    player.playPrevTrack = function() {
+      var current = layers.find('.mejs-playlist > ul > li.current');
+      /*var notplayed = layers.find('.mejs-playlist > ul > li').not('.played');
+      if (notplayed.length < 1) {
+        current.removeClass('played').siblings().removeClass('played');
+        notplayed = layers.find('.mejs-playlist > ul > li').not('.played');
+      }*/
+      var prev = current.prev();
+      if (prev.length < 1 && player.options.loop) {
+        prev = current.siblings().last();
+      }
+      if (prev.length == 1) {
+        current.removeClass('played');
+        prev.addClass('current').siblings().removeClass('current');
+        player.playTrack(prev.attr('data-url'));
+      }
+    };
+    player.playTrack = function(url) {
+      player.pause();
+      player.setSrc(url);
+      player.load();
+      player.play();
+    };
+    // when current track ends - play the next one
+    media.addEventListener('ended', function(e) {
+      player.playNextTrack();
+    }, false);
+  }
+})(jQuery);
